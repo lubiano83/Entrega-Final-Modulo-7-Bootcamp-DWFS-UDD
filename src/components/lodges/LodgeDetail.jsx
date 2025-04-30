768a410208a8fd2e1b85941ad56252af35d7c833
lubiano83 <josepablolubiano@lubiano83.local>
2025-04-29 21:17:38 -0400

    terminando de ajustar lodgeDetail

--- src/components/lodges/LodgeDetail.jsx
+++ src/components/lodges/LodgeDetail.jsx
@@ -23,8 +23,8 @@ export default function LodgeDetail() {
 
 
     return (
-        <div className="bg-amber-100 p-4 flex flex-col w-2/3 min-w-72 h-auto lg:w-auto xl:flex-row justify-center items-center gap-4 rounded-xl shadow-sm shadow-amber-950">
-                <div className="relative aspect-video min-w-56 max-h-md lg:h-100">
+        <div className="bg-amber-100 p-4 flex flex-col w-2/3 min-w-72 h-auto xl:flex-row justify-center items-center gap-4 rounded-xl shadow-sm shadow-amber-950">
+                <div className="relative aspect-video min-w-56 w-full lg:h-full">
                     <Link to={lodge.image[currentImageIndex]} target="_blank" className="aspect-video group relative h-full block w-full">
                         <img src={lodge.image[currentImageIndex]} alt="Imagen del lodge" className="h-full w-full" />
                         <span className="absolute inset-0 flex justify-center items-center text-amber-950 font-bold text-lg opacity-0 group-hover:opacity-80 bg-white bg-opacity-50 transition-opacity duration-300">Agrandar Imagen</span>
@@ -32,8 +32,8 @@ export default function LodgeDetail() {
                     <button onClick={prevImage} className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-amber-500 text-amber-950 px-2 py-1 rounded-r"> <SvgImage src={"/arrow-sm-left-svgrepo-com.svg"} /> </button>
                     <button onClick={nextImage} className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-amber-500 text-amber-950 px-2 py-1 rounded-l"> <SvgImage src={"/arrow-sm-right-svgrepo-com.svg"} /> </button>
                 </div>
-                <div className="flex flex-col justify-between items-start gap-4 md:gap-8 h-full w-auto">
-                    <div className="flex flex-col justify-between items-start text-lg h-full w-full">
+                <div className="flex flex-col justify-between items-start gap-4 h-full w-full xl:w-auto">
+                    <div className="flex flex-col justify-between items-start text-lg h-full 2xl:h-10/12">
                         <p className="truncate overflow-hidden whitespace-nowrap"><strong>Id: </strong>{lodge._id}</p>
                         <p><strong>Nombre: </strong>{lodge.hotel}</p>
                         <p><strong>Piezas: </strong>{lodge.bedroom}</p>
