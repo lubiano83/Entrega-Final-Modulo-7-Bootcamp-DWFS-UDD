d28aa63de8df39769746d08a9c6f44e2a47571f7
lubiano83 <josepablolubiano@lubiano83.local>
2025-05-01 19:13:14 -0400

    ajustes varios

--- src/components/admin/ReservationFinish.jsx
+++ src/components/admin/ReservationFinish.jsx
@@ -23,16 +23,15 @@ export default function ReservationFinish() {
         <div className="flex flex-col justify-center items-center w-full">
             <form onSubmit={handleSubmit} className="bg-amber-100 rounded-xl p-4 xl:flex flex-col justify-center items-center gap-4 min-w-72 w-1/2 shadow-sm shadow-amber-950 max-w-xl hidden">
                 <Message className="flex xl:hidden">Vista no disponible en dispositivos moviles..</Message>
-                <Title>Reserva Finalizada:</Title>
-                <div className="flex justify-center items-center gap-2 text-xl">
-                    <p>Finalizar Reserva:</p>
+                <div className="flex justify-center items-center gap-2">
+                    <Title>Finalizar Reserva:</Title>
                     <input type="checkbox" onChange={() => setPaid(!paid)} className="w-5 h-5" />
                 </div>
                 <div className="flex justify-center items-center gap-2">
                     <Link to={"/admin/reservations"}>
                         <Boton>Volver</Boton>
                     </Link>
-                    <Boton type="submit">Finalizar</Boton>
+                    <Boton type="submit">Registrar</Boton>
                 </div>
             </form>
         </div>
