b00936c1a830bed31bd9b20d01486eb462ff869e
lubiano83 <josepablolubiano@lubiano83.local>
2025-05-02 13:20:59 -0400

    dejando las cabañas visibles

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -16,7 +16,7 @@ export default function Menu() {
         <div className="flex 2xl:flex-col 2xl:h-full justify-around items-center 2xl:w-56 w-full gap-2 bg-amber-500 2xl:bg-amber-700 text-white p-1">
             <Link to={"/"}><p className="cursor-pointer hover:text-amber-950">Inicio</p></Link>
             { logged ? <Link to={"/admin"}><p className="cursor-pointer hover:text-amber-950">Admin</p></Link> : "" }
-            { logged ?  <Link to={"/lodges"}><p className="cursor-pointer hover:text-amber-950">Lodges</p></Link> : "" }
+            <Link to={"/lodges"}><p className="cursor-pointer hover:text-amber-950">Lodges</p></Link>
             { logged ? "" : <Link to={"/login"}><p className="cursor-pointer hover:text-amber-950">Login</p></Link> }
             { logged ? "" : <Link to={"/register"}><p className="cursor-pointer hover:text-amber-950">Register</p></Link> }
             { logged ? <p className="cursor-pointer hover:text-amber-950" onClick={() => handleLogout()}>Logout</p> : "" }
