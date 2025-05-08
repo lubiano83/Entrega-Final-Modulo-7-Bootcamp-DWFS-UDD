9baa3666d3e36b4ca65cfeaecdb2072292e3a08f
lubiano83 <josepablolubiano@MacBook-Air-de-Jose.local>
2025-05-08 13:57:31 -0400

    ajustes varios

--- src/pages/admin/CreateLodgePage.jsx
+++ src/pages/admin/CreateLodgePage.jsx
@@ -14,7 +14,7 @@ export default function CreateLodgePage({ isDarkMode }) {
             setLogged(false);
             navigate("/");
         }
-    }, [logged, navigate]);
+    }, [logged, navigate, token]);
 
     return (
         <div className={`${isDarkMode ? "text-amber-950" : "text-green-950"} h-full w-full flex flex-col justify-center items-center p-8 gap-4`}>
--- src/pages/admin/YourLodgesPage.jsx
+++ src/pages/admin/YourLodgesPage.jsx
@@ -13,7 +13,7 @@ export default function YourLodgesPage({ isDarkMode }) {
             setLogged(false);
             navigate("/");
         }
-    }, [logged, navigate]);
+    }, [logged, navigate, token]);
 
     return (
         <div className={`${isDarkMode ? "text-amber-950" : "text-green-950"} h-full w-full flex flex-col justify-center items-center p-8 gap-4`}>
