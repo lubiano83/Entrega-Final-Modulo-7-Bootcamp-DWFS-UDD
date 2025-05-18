7e5b5be36979db83b0dcc5bffedd2cd45388e03a
lubiano83 <josepablolubiano@lubiano83-2.local>
2025-05-18 11:37:11 -0400

    ajustes varios

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -167,11 +167,9 @@ export const AuthProvider = ({ children }) => {
                 const data = await response.json();
                 setLogged(true);
                 setToken(data.token);
-                return data;
             } else {
                 setToken(null);
                 setLogged(false);
-                return null;
             }
         } catch (error) {
             console.log("Error al verificar la sesión:", error.message);
